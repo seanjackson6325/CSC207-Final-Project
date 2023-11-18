6d50c4354e4c4530c27ad60fd7ca95794a65d777
kylej <kylejoung23@gmail.com>
2023-11-17 23:48:55 -0500

    DeleteTodo Controller / Use Case done. DeleteTodo Presenter, and Factory (front end work) needs to be done

---
+++ src/use_case/DeleteTodo/DeleteTodoInteractor.java
@@ -0,0 +1,43 @@
+package use_case.DeleteTodo;
+
+import app.EntityMemory;
+import data_access.DataAccessInterface;
+
+
+public class DeleteTodoInteractor implements DeleteTodoInputBoundary {
+
+    private final DeleteTodoOutputBoundary deleteTodoPresenter;
+    private final DataAccessInterface dataAccess;
+
+    public DeleteTodoInteractor(DataAccessInterface dataAccess,
+                                DeleteTodoOutputBoundary deleteTodoPresenter) {
+        this.dataAccess = dataAccess;
+        this.deleteTodoPresenter = deleteTodoPresenter;
+    }
+
+    @Override
+    public void execute(DeleteTodoInputData deleteTodoInputData) {
+
+        try {
+            // remove to-do
+            deleteTodoInputData.getList().remove(deleteTodoInputData.getIndex());
+
+            // update the change in the Database
+            if (deleteTodoInputData.getTeam() == null) {
+                dataAccess.updateUser(EntityMemory.getLoggedInUser());
+            }
+            else {
+                dataAccess.updateTeam(deleteTodoInputData.getTeam());
+            }
+
+            deleteTodoPresenter.prepareSuccessView(new DeleteTodoOutputData("Successfully Deleted Todo From List"));
+
+        }
+        catch (Exception e) {
+            deleteTodoPresenter.prepareFailView("Failed to Delete Todo From List");
+        }
+
+
+
+    }
+}
